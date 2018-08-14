1813b947c3306a9ba66bc0f8db9b5e272a474410
Ed Jeon <muikor@gmail.com>
2018-08-14 17:56:46 +0900

    no reuse remote channel

--- remote_embeddings.py
+++ remote_embeddings.py
@@ -13,37 +13,40 @@ class ImageEmbeddingService:
         self.host = host
         self.load_balancing = load_balancing
         self._timeout = timeout
-        self._stub = self._get_stub()
 
-        response = self._stub.Dimension(pb2.Empty(), timeout=1)
+        with self._channel() as channel:
+            stub = pb2_grpc.ImageEmbeddingStub(channel)
+            response = stub.Dimension(pb2.Empty(), timeout=1)
         self._dim = response.dim
 
-    def _get_stub(self):
+    def _channel(self):
         if self.load_balancing:
-            self._channel = grpc.insecure_channel(self.host, [("grpc.lb_policy_name", "round_robin")])
+            return grpc.insecure_channel(self.host, [("grpc.lb_policy_name", "round_robin")])
         else:
-            self._channel = grpc.insecure_channel(self.host)
-        return pb2_grpc.ImageEmbeddingStub(self._channel)
+            return grpc.insecure_channel(self.host)
 
     def dim(self):
         return self._dim
 
     def get_embedding(self, url, is_retry=False):
         try:
-            response = self._stub.Embedding(pb2.EmbeddingRequest(url=url), timeout=self._timeout)
+            with self._channel() as channel:
+                stub = pb2_grpc.ImageEmbeddingStub(channel)
+                response = stub.Embedding(pb2.EmbeddingRequest(url=url), timeout=self._timeout)
         except grpc.RpcError as e:
             msg = str(e)
             if 'Forbidden' in msg:
                 return None
             if not is_retry:
                 logging.warn("embedding error: %s", msg)
-                self._stub = self._get_stub()
                 return self.get_embedding(url, is_retry=True)
             raise e
         return np.array(response.embedding, dtype=np.float32)
 
     def info(self):
-        response = self._stub.Info(pb2.Empty(), timeout=1)
+        with self._channel() as channel:
+            stub = pb2_grpc.ImageEmbeddingStub(channel)
+            response = stub.Info(pb2.Empty(), timeout=1)
         return response.message
 
     def stop(self):
